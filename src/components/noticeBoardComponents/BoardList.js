e09be82d0e822d541e2fda21bbd7ba4dcc5226db
wjdsheep15 <wjd15sheep@gmail.com>
2024-04-13 08:31:06 +0900

    Feat: add delete, update

--- src/components/noticeBoardComponents/BoardList.js
+++ src/components/noticeBoardComponents/BoardList.js
@@ -1,6 +1,42 @@
+'use client'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+import { useEffect, useState } from 'react'
 export default function BoardList({ affectionData }) {
-  console.log(affectionData)
+  const [content, setContent] = useState('')
+  const [chose, setChose] = useState()
+  const [title, setTitle] = useState('')
+  const [affectionId, setAffectionId] = useState()
+  const [chnage, setChange] = useState(false)
+  const router = useRouter()
+
+  const deleteHandler = async (id) => {
+    const response = await fetch(`http://localhost:3000/api/skateboardSwagger/${id}`, {
+      method: 'DELETE',
+    })
+  }
+
+  const changeHandler = () => {
+    setChange(true)
+  }
+
+  const handleSubmit = async (event) => {
+    const formData = new FormData(event.target)
+    const id = affectionId
+    const response = await fetch(`http://localhost:3000/api/skateboardSwagger/${id}`, {
+      method: 'PUT',
+      body: formData,
+    })
+
+    const result = await response.json()
+    const inner = result
+
+    // setResultResponse(inner.data)
+
+    router.refresh()
+  }
+
   return (
     <>
       {affectionData.map((index, key) => (
@@ -17,13 +53,60 @@ export default function BoardList({ affectionData }) {
             <span className='text-base text-lg text-black'>{index.title}</span>
             <span className='text-base text-lg text-black'>{index.content}</span>
           </div>
-          <button>
+          <button
+            className='h-full w-12'
+            onClick={() => {
+              setChose(index.affectionPostId)
+              deleteHandler(index.affectionPostId)
+            }}
+          >
             <span>삭제</span>
           </button>
 
-          <button>
+          <button
+            className='h-full w-12'
+            onClick={() => {
+              changeHandler()
+              setChose(index.affectionPostId)
+              setAffectionId(index.affectionPostId)
+            }}
+          >
             <span>수정</span>
           </button>
+
+          {chnage && chose === index.affectionPostId && (
+            <div>
+              <form onSubmit={handleSubmit} className='flex '>
+                <div>
+                  <input
+                    className='ml-2 h-10  w-full text-base text-sm focus:outline-none'
+                    type='text'
+                    key={index.affectionPostId}
+                    id='title'
+                    name='title'
+                    value={title}
+                    onChange={(event) => {
+                      setTitle(event.target.value)
+                    }}
+                    placeholder='제목 입력'
+                  />
+                  <input
+                    className='ml-2 h-10  w-full text-base text-sm focus:outline-none'
+                    type='text'
+                    key={index.affectionPostId + 10}
+                    id='content'
+                    name='content'
+                    value={content}
+                    onChange={(event) => {
+                      setContent(event.target.value)
+                    }}
+                    placeholder='내용 입력'
+                  />
+                </div>
+                <button type='submit'>저장</button>
+              </form>
+            </div>
+          )}
         </div>
       ))}
     </>
