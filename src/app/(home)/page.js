e6ee9809e5999b27ba211b5067c5b14bd58372b9
wjdsheep15 <wjd15sheep@gmail.com>
2024-04-12 16:11:26 +0900

    Fix: Add KakaoMap | prettierrc config

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,3 +1,14 @@
+import KakaoMap from '../components/KakaoMap'
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <div className='flex '>
+        <div>Kakao map</div>
+        <main className='absolute left-0 top-20 h-screen w-screen'>
+          <KakaoMap />
+        </main>
+      </div>
+    </div>
+  )
 }
