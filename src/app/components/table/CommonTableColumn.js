95d4900c4757fc1bd76108c1294fb1331784063a
rayrayr <rayrayr@example.com>
2024-04-13 05:50:23 +0900

    Feat: Affection Post

---
+++ src/app/components/table/CommonTableColumn.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CommonTableColumn = ({ children }) => {
+  return <td className='p-2'>{children}</td>
+}
+
+export default CommonTableColumn
---
+++ src/app/components/table/CommonTableRow.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CommonTableColumn = ({ children }) => {
+  return <td className='hover: white cursor-pointer bg-gray-200'>{children}</td>
+}
+
+export default CommonTableRow
